4265ce2a021c63b48d1e14fda8acf7cfc2ebf4d6
Christian Mladenov <cmlad@cmlad.com>
2026-08-27 21:52:54 -0700

    Move clipboard picker activation mode

--- Sources/ShortyCore/ClipboardPickerSearch.swift
+++ Sources/ShortyCore/ClipboardPickerSearch.swift
@@ -5,24 +5,6 @@ public enum ClipboardPickerMode: Sendable {
     case snippetsOnly
 }
 
-public enum ClipboardPickerActivationMode: Equatable, Sendable {
-    case standard
-    case plainText
-    case joinedLines
-
-    public static func fromModifiers(command: Bool, shift: Bool) -> ClipboardPickerActivationMode {
-        if shift {
-            return .joinedLines
-        }
-
-        if command {
-            return .plainText
-        }
-
-        return .standard
-    }
-}
-
 public struct ClipboardPickerFolder: Equatable, Identifiable, Sendable {
     public enum Kind: Sendable {
         case history
